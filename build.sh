2d3d18c6ef14a48f5591dae003a4ffb5e0a119e2
Timo Eberl <timo@timoeberl.de>
2025-09-20 15:56:30 +0200

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# HEAPU8: expose memory to JS to read image data
+# -O3: Max runtime optimizations
+
 emcc src/ray_tracer.c \
 	-o src/ray_tracer.js \
 	-sMODULARIZE=1 \
 	-sEXPORT_ES6=1 \
 	--emit-tsd ray_tracer.d.ts \
-	-sEXPORTED_RUNTIME_METHODS=cwrap,HEAPU8 \
-	-sEXPORTED_FUNCTIONS=_render_full,_render_fast \
+	-sEXPORTED_RUNTIME_METHODS=HEAPU8 \
 	-O3
--- build_debug.sh
+++ build_debug.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# HEAPU8: expose memory to JS to read image data
+# -g: Preserve debug information.
+# -O1: Simple optimizations that make the code way faster (~6x) while preserving debug information
+#      If debugging breaks, try removing it
+
 emcc src/ray_tracer.c \
 	-o src/ray_tracer.js \
 	-sMODULARIZE=1 \
 	-sEXPORT_ES6=1 \
 	--emit-tsd ray_tracer.d.ts \
-	-sEXPORTED_RUNTIME_METHODS=cwrap,HEAPU8 \
-	-sEXPORTED_FUNCTIONS=_render_full,_render_fast,_malloc,_free \
+	-sEXPORTED_RUNTIME_METHODS=HEAPU8 \
 	-g \
-	-gsource-map
+	-gsource-map \
+	-O1
