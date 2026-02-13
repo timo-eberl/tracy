7688decf6d28a69a4867531ef3ebafde6eb40a46
Tilman Zorn <tilman.zorn@outlook.de>
2026-02-13 13:33:17 +0100

    FIX bug

--- tests/metrics/rmse/rmse_test.zig
+++ tests/metrics/rmse/rmse_test.zig
@@ -3,13 +3,15 @@ const testing = std.testing;
 const exr_utils = @import("exr_utils");
 const rmse = @import("rmse.zig");
 
+const stdout = std.io.getStdOut().writer();
+
 test "RMSE test" {
     //const ally = testing.allocator;
     // const ref_fp = "../../img/exr/reference.exr";
     // const target_fp = "../../../render_c.exr";
 
     //const score = rmse.compareAndLog(ally, ref_fp, target_fp);
-    //std.debug.print(score, .{});
+    //stdout.print(score, .{});
 
     // TODO DELETE and uncomment above
     // 1. Initialize a seed (using the current time)
@@ -24,6 +26,5 @@ test "RMSE test" {
     // 4. Generate a float between 0.0 and 1.0
     // float(f32) or float(f64) both work
     const my_rand = random.float(f32);
-
-    std.debug.print("{d:.4}", .{my_rand});
+    stdout.print("{d:.4}", .{my_rand});
 }
