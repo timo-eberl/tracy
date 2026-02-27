f0f806648e311a70a6eb47770e74e379dc40bb5f
Timo Eberl <timo@timoeberl.de>
2026-02-27 20:22:25 +0100

    Fix vite build (force vite to bundle workers as ES modules)

--- examples/web/vite.config.ts
+++ examples/web/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  // Emscripten's multithreading outputs Web Workers using ES module syntax.
+  // Vite defaults to classic 'iife' workers which break on top-level await.
+  // Setting this to 'es' forces Vite to bundle workers as native ES modules.
+  worker: {
+    format: 'es',
+  },
   server: {
     watch: {
       // Tell Vite's file watcher to ignore all c and wasm files.
